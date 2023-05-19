1359f8e656150618d20eb2886ca9c2b906d7372a
sktvera <veeraa1270@gmail.com>
2023-05-19 11:35:34 -0500

    new path in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom'
-import { Banner } from "./Components/Banner/Banner";
-import { Skills } from "./Components/Skills/Skills";
-import { Projects } from "./Components/Projects/Projects";
-import { Contact } from "./Components/Contact";
-import { Footer } from "./Components/Footer";
 import Welcome from './Routes/Welcome/Welcome'
 
 function App() {
