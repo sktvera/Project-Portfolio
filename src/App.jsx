333fe25605d1060a0b34ee130fb3667dba3d147b
sktvera <veeraa1270@gmail.com>
2023-05-19 11:07:06 -0500

    recat router

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,21 @@ import { Skills } from "./Components/Skills/Skills";
 import { Projects } from "./Components/Projects/Projects";
 import { Contact } from "./Components/Contact";
 import { Footer } from "./Components/Footer";
+import Welcome from './Routes/Welcome/Welcome'
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+      <Route path='/' element={<Welcome/>}/>
+      <Route path='/personal-portfolio' element={<Welcome/>}/>
+      </Routes>
       <Banner />
       <Skills />
       <Projects />
       <Contact />
       <Footer />
+      
     </div>
   );
 }
